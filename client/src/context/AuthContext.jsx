130123c288d7e8f15294c18cf9cec01cddec072a
22cs052devpatel <120241254+22cs052devpatel@users.noreply.github.com>
2025-04-23 16:11:29 +0530

    Fix path to dist folder

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,4 +1,5 @@
-import React, { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+
 export const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
