43c1c699f53831f90945cae62db443c14b215e0d
22cs052devpatel <120241254+22cs052devpatel@users.noreply.github.com>
2025-04-23 11:32:44 +0530

    Fix Not Found error for React routes

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  // baseURL: "http://localhost:8800/api",
-  baseURL: "https://real-estate-dev-1.onrender.com", // Replace with your Render backend URL
+  baseURL: process.env.NODE_ENV === "development"
+    ? "http://localhost:8800/api" // Local development URL
+    : "https://real-estate-dev-1.onrender.com/api", // Production URL
   withCredentials: true,
 });
 
