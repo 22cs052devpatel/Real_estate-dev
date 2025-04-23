f622742b73dc949c70ac7681d85edd4d383d6ea0
22cs052devpatel <120241254+22cs052devpatel@users.noreply.github.com>
2025-04-23 12:19:42 +0530

    Fix path to dist folder

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -56,8 +56,8 @@ import { useNotificationStore } from "../../lib/notificationStore";
           <a href="/">About</a>
           <a href="/">Contact</a>
           <a href="/">Agents</a>
-          <a href="/">Sign in</a>
-          <a href="/">Sign up</a>
+          <a href="/login">Sign in</a>
+          <a href="/register">Sign up</a>
         </div>
       </div>
     </nav>
